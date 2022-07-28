9a29631f97a2d5cc77e03ef2da6d2f9f8b3cfd24
Hmlaast <110089751+Hmlaast@users.noreply.github.com>
2022-07-28 12:21:27 +0530

    Create alive.js

---
+++ plugins/alive.js
@@ -0,0 +1,14 @@
+const{tiny}=require('../lib')
+module.exports = {
+  name: "alive",
+  alias: ["live", "bot"],
+  category: "info",
+  desc: "Alive Msg",
+  async mbb({ msg }) {
+    const start = new Date().getTime()
+		await msg.reply(tiny('Hi ${msg.pushName}/n I'm Alive'))
+		const end = new Date().getTime()
+		return await msg.reply(tiny('Type .menu for commands')
+		)
+  },
+};
