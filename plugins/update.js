9b0840570db2e99d81ae1d97b377d267f562c33f
Hmlaast <110089751+Hmlaast@users.noreply.github.com>
2022-07-28 13:31:25 +0530

    Rename find.js to update.js

--- plugins/update.js
+++ plugins/update.js
@@ -1,24 +1,68 @@
-let { execSync } = require('child_process')
-let handler = async (m, { conn, text, isROwner }) => {
-  if (global.conn.user.jid == conn.user.jid) {
-    let stdout = execSync('git remote set-url origin https://github.com/HMser/testlast.git && git pull' + (isROwner && text ? ' ' + text : ''))
-    if (isROwner) require('fs').readdirSync('plugins').map(v => global.reload('', v))
-    m.reply(stdout.toString())
-  }
-}
-handler.help = ['update']
-handler.tags = ['host']
-handler.command = /^update$/i
-handler.owner = true
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
+const Heroku = require("heroku-client");
+let { tiny } = require("../lib");
+let config = require("../config");
+const heroku = new Heroku({ token: config.HEROKU_API_KEY });
+const simpleGit = require("simple-git");
+const git = simpleGit();
+module.exports = {
+  name: "update",
+  category: "owner",
+  desc: "Check for updates",
+  isOwner:true,
+  async mbb({ msg, conn }, { q, prefix }) {
+    await git.fetch();
+    var commits = await git.log([config.BRANCH+ "..origin/" + config.BRANCH]);
+    if (q === "now") {
+      if (commits.total === 0)
+        return await conn.sendMessage(msg.from, {
+          text: tiny("You have the latest version installed"),
+        });
+      var app = await heroku.get("/apps/" + config.HEROKU_APP_NAME);
+      await conn.sendMessage(msg.from, { text: "*ᴜᴘᴅᴀᴛɪɴɢ...*" });
+      git.fetch("upstream", config.BRANCH);
+      git.reset("hard", ["FETCH_HEAD"]);
+      var git_url = app.git_url.replace(
+        "https://",
+        "https://api:" + config.HEROKU_API_KEY + "@"
+      );
+      try {
+        try {
+          await git.addRemote("heroku", git_url);
+        } catch (e) {
+          console.log(e);
+        }
 
-handler.admin = false
-handler.botAdmin = false
+        await git.push("heroku", config.BRANCH);
 
-handler.fail = null
-handler.exp = 0
+        await conn.sendMessage(msg.from, { text: "*Updated...*" });
 
-module.exports = handler
+        await conn.sendMessage(msg.from, { text: "Restarting..." });
+      } catch (e) {
+        msg.reply(e);
+      }
+    }
+
+    if (commits.total === 0) {
+      await conn.sendMessage(msg.from, {
+        text: tiny("You have the latest version installed"),
+      });
+    } else {
+      var availupdate = "*ᴜᴘᴅᴀᴛᴇs ᴀᴠᴀɪʟᴀʙʟᴇ* \n\n";
+      commits["all"].map((commit, num) => {
+        availupdate += num + 1 + " ●  " + tiny(commit.message) + "\n";
+      });
+      await conn.sendMessage(msg.from, {
+        text: availupdate,
+        footer: tiny("click here to update"),
+        buttons: [
+          {
+            buttonId: `${prefix}update now`,
+            buttonText: { displayText: tiny("update now") },
+            type: 1,
+          },
+        ],
+        headerType: 1,
+      });
+    }
+  },
+};
